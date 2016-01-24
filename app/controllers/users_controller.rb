6fab6099a72e93b7336f736dad008e0a5af4ffc7
John LaGreco <johnlagreco@gmail.com>
2016-01-24 15:51:41 -0500

    Fixed new found errors & added background image for homepage.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,14 @@ class UsersController < ApplicationController
 	  end
 
 	  def create
-	  	@user = User.create(user_params)
-	  	redirect_to @user
+	  	@user = User.new(user_params)
+		  if @user.save
+		  session[:user_id] = @user.id
+		  redirect_to login_path
+		  else
+		  redirect_to :back
+		  end
+
 	  end
 
 	  private
